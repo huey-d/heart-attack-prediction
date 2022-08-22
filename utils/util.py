c10bf127d1f838dd0be3a1c2709f8ac88b7eb2c0
yo-huey <huydn07@gmail.com>
2022-08-21 19:01:12 -0700

    adding txt file & changing folder name

--- utils/util.py
+++ utils/util.py
@@ -4,10 +4,8 @@ import os
 def make_dirs():
     data_path = "data/"
     output_path = "output/"
-    model_file = "final_model/"
+    model_file = "model_file/"
         
     for _ in [data_path, output_path, model_file]:
         if not os.path.exists(_):
             os.mkdir(_)
-
-
