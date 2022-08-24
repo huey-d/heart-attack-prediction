e128de662639af9aec48f86cc869735d412422b8
Huey <huydn07@gmail.com>
2022-08-24 02:55:27 -0700

    rechanged for pipeline testing

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,9 +24,20 @@ class TestDirectories(unittest.TestCase):
         src_models = "src/models"
         src_visuals = "src/visualization"
 
-        for path in [utils_path, data_ext, data_int, data_proc, data_raw, model_path, references_path, reports_path, test_path, src_data, src_features, src_models, src_visuals]:
-            self.assertTrue(os.path.exists(path))
-        
+        self.assertTrue(os.path.exists(utils_path))
+        self.assertTrue(os.path.exists(data_ext))
+        self.assertTrue(os.path.exists(data_int))
+        self.assertTrue(os.path.exists(data_proc))
+        self.assertTrue(os.path.exists(data_raw))
+        self.assertTrue(os.path.exists(model_path))
+        self.assertTrue(os.path.exists(references_path))
+        self.assertTrue(os.path.exists(reports_path))
+        self.assertTrue(os.path.exists(test_path))
+        self.assertTrue(os.path.exists(src_data))
+        self.assertTrue(os.path.exists(src_features))
+        self.assertTrue(os.path.exists(src_models))
+        self.assertTrue(os.path.exists(src_visuals))
+
     def tearDown(self):
         pass
     
