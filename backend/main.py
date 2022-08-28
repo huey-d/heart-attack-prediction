1c799e7930ba859bf7dad2335284acaa2bbcca89
Huey <huydn07@gmail.com>
2022-08-27 22:59:59 -0700

    finished backend

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,10 @@
 import json
 import pickle
-
 from fastapi import Body, FastAPI
-from fastapi.logger import logger
 from pydantic import BaseModel
 import uvicorn
-
 import xgboost
+import requests
 
 app = FastAPI(title="Heart Attack API", description="API for Heart Attack Classification")
 
@@ -26,6 +24,10 @@ class HeartAttack(BaseModel):
     caa : int
     thall : int
 
+# homepage route
+@app.get("/")
+def read_root():
+	return {'message': 'This is the homepage of the API '}
 
 # Loading trained model
 with open('./deploy_xgboost.pkl', "rb") as f:
@@ -36,8 +38,8 @@ with open('./deploy_xgboost.pkl', "rb") as f:
 # We should expect a JSON response with heart attack risk classified.
 
 # Columns are: ['age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 'slp', 'caa', 'thall']
-@app.post('/prediction')
-def get_risk(data: HeartAttack):
+@app.post('/predict')
+async def get_risk(data: HeartAttack):
     received = data.dict()
     age = received['age']
     sex = received['sex']
@@ -52,46 +54,12 @@ def get_risk(data: HeartAttack):
     slp = received['slp']
     caa = received['caa']
     thall = received['thall']
-    pred_name = loaded_model.predict([[age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall]]).tolist()[0]
-    return  {'Prediction' : pred_name}
     
-
-# @app.post('/subscription')
-# def subscription(data :dict = Body(...)):
-#     #output_data=await data.json()
-#     print(data)
-
-
-# @app.get("/get_entities/{id}")
-# async def get_entities(id:str ):
+    features = [[age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall]]
+    pred_name = loaded_model.predict(features).tolist()[0]
+    if pred_name == 1:
+        pred_name = 'Higher Risk of Heart Attack'
+    else:
+        pred_name = 'Lower Risk of Heart Attack'
     
-#     url="http://orion.docker:1026/ngsi-ld/v1/entities/" +  id + "?options=keyValues"
-
-#     client = httpx.Client()
-#     response = client.get(url)
-    
-#     logger.info(response.json())
-#     return response.json()
-
-# @app.patch("/prediction/{id}/{potability}")
-# def notify_prediction(id:str,potability:str):
-#     url = "http://orion.docker:1026/ngsi-ld/v1/entities/" + id + "/attrs/Potability"
-
-#     payload = json.dumps({
-# 	"value": potability,
-# 	"type": "Property"
-# 	})
-#     headers = {
-# 	'Content-Type': 'application/json',
-# 	'Link': '<http://context/ngsi-context.jsonld>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json"'
-# 	}
-#     client = httpx.Client()
-#     response = client.patch(url, headers=headers, data=payload)
-
-#     return response.json()
-
-
-# homepage route
-@app.get("/")
-def read_root():
-	return {'message': 'This is the homepage of the API '}
+    return pred_name
