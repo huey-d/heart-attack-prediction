49e8501e6b6e94ec9a6464ac7ba4400e20afe9c6
Huey <huydn07@gmail.com>
2022-08-27 23:00:36 -0700

    creating docker compose

--- frontend/app.py
+++ frontend/app.py
@@ -3,6 +3,17 @@ import streamlit as st
 import pickle
 import xgboost
 import os
+import requests
+import json
+
+# async def fetch(session, url):
+#     try:
+#         async with session.get(url) as response:
+#             result = await response.json()
+#             return result
+#     except Exception:
+#         return {}
+
 
 # ********************************* Start HTML config ****************************************
 
@@ -19,56 +30,48 @@ st.markdown(s, unsafe_allow_html=True)
 
 # ******************************* End HTML configuration ****************************************
 
-def load_model_and_predict(dir_path: str = 'backend/', model_path: str = 'deploy_xgboost.pkl'):
-	with open(os.path.join(dir_path, model_path), "rb") as f:
-		loaded_model = pickle.load(f)
+# def load_model_and_predict(dir_path: str = 'backend/', model_path: str = 'deploy_xgboost.pkl'):
+# 	with open(os.path.join(dir_path, model_path), "rb") as f:
+# 		loaded_model = pickle.load(f)
 			
-	return loaded_model 
+# 	return loaded_model 
 
-def predict(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall):
-	prediction = model.predict(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)
-	return prediction
+# def predict(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall):
+# 	prediction = model.predict(age, sex, cp, trtbps, chol, fbs, restecg, thalachh, exng, oldpeak, slp, caa, thall)
+# 	return prediction
 
 
 def run():
-	
-	features = []
 
 	st.title("Heart Attack Risk Assessment App")
 
 	age = st.number_input("How old are you", 20, 80)
-	features.append(age)
 
-	gender = st.radio('Gender?', ("Male", "Female"))
-	if gender == 'Female':
-		gender = 0
+	sex = st.radio('Gender?', ("Male", "Female"))
+	if sex == 'Female':
+		sex = 0
 	else:
-		gender = 1
-	features.append(gender)
+		sex = 1
 
-	chest_pain = st.radio('Describe Chest Pain', ("None", "Moderate", "Severe", "Extreme"))
-	if chest_pain == 'None':
-		chest_pain = 0
-	elif chest_pain == 'Moderate':
-		chest_pain = 3
-	elif chest_pain == 'Severe':
-		chest_pain = 1
-	elif chest_pain == 'Extreme':
-		chest_pain = 2
-	features.append(chest_pain)
+	cp = st.radio('Describe Chest Pain', ("None", "Moderate", "Severe", "Extreme"))
+	if cp == 'None':
+		cp = 0
+	elif cp == 'Moderate':
+		cp = 3
+	elif cp == 'Severe':
+		cp = 1
+	elif cp == 'Extreme':
+		cp = 2
 
 	trtbps = st.slider('Resting Blood Pressure (in mm Hg)', 94, 200, 1)
-	features.append(trtbps)
 
 	chol = st.slider('Serum cholestoral (in mg/dl)', 126, 564, 1)
-	features.append(chol)
 
 	fbs = st.radio('Is fasting blood higher than 120 mg/dL', ["True", "False"])
 	if fbs == 'True':
 		fbs = 1
 	elif fbs == 'False':
 		fbs = 0
-	features.append(fbs)
 
 	restecg = st.radio('Resting Electrocardiographic Results', ["Normal", "Wave abnormality", "Hypertrophy"])
 	if restecg == "Normal":
@@ -77,63 +80,53 @@ def run():
 		restecg = 2
 	elif restecg == "Hypertrophy":
 		restecg = 0
-	features.append(restecg)
 	
-	thalach = st.slider("Maximum Heart Rate", 71, 202, 1)
-	features.append(thalach)
+	thalachh = st.slider("Maximum Heart Rate", 71, 202, 1)
 
-	exang = st.radio("Exercise induced angina", ["Yes", "No"])
-	if exang == "Yes":
-		exang = 1
-	elif exang == "No":
-		exang = 0
-	features.append(exang)
+	exng = st.radio("Exercise induced angina", ["Yes", "No"])
+	if exng == "Yes":
+		exng = 1
+	elif exng == "No":
+		exng = 0
 
 	oldpeak = st.slider('ST depression induced by exercise relative to rest', 0.0, 6.2, 0.1)
-	features.append(oldpeak)
 
-	slope = st.radio("Slope of peak exercise", ["Upsloping", "Flat", "Downsloping"])
-	if slope == "Downsloping":
-		slope = 0
-	elif slope == "Flat":
-		slope = 1
-	elif slope == "Upsloping":
-		slope = 2
-	features.append(slope)
+	slp = st.radio("Slope of peak exercise", ["Upsloping", "Flat", "Downsloping"])
+	if slp == "Downsloping":
+		slp = 0
+	elif slp == "Flat":
+		slp = 1
+	elif slp == "Upsloping":
+		slp = 2
 
-	ca = st.slider("Number of major vessels", 0, 4, 1)
-	features.append(ca)
+	caa = st.slider("Number of major vessels", 0, 4, 1)
 
-	thal = st.radio("Number of major vessels", ["Normal", "Fixed defect", "Reversable Defect"])
-	if thal == "Normal":
-		thal = 0
-	elif thal == "Flat":
-		thal = 1
-	elif thal == "Upsloping":
-		thal = 2
-	features.append(thal)
-	
+	thall = st.radio("Number of major vessels", ["Normal", "Fixed defect", "Reversable Defect"])
+	if thall == "Normal":
+		thall = 0
+	elif thall == "Flat":
+		thall = 1
+	elif thall == "Upsloping":
+		thall = 2
+
+	data = {'age':age,
+			'sex': sex,
+			'cp': cp,
+			'trtbps': trtbps,
+			'chol': chol,
+			'fbs': fbs,
+			'restecg': restecg,
+			'thalachh': thalachh,
+			'exng': exng,
+			'oldpeak': oldpeak,
+			'slp': slp,
+			'caa': caa,
+			'thall': thall}
+
 	if st.button("Predict"):
-		output = predict(features)
-		
-		st.write(" " + output + ' category!')
-      
-  
-# def main():       
-#     # following lines create boxes in which user can enter data required to make prediction 
-    
-#     Married = st.selectbox('Marital Status',("Unmarried","Married")) 
-#     ApplicantIncome = st.number_input("Applicants monthly income") 
-#     LoanAmount = st.number_input("Total loan amount")
-#     Credit_History = st.selectbox('Credit_History',("Unclear Debts","No Unclear Debts"))
-#     result =""
-      
-#     # when 'Predict' is clicked, make the prediction and store it 
-#     if st.button("Predict"): 
-#         result = prediction(Gender, Married, ApplicantIncome, LoanAmount, Credit_History) 
-#         st.success('Your loan is {}'.format(result))
-#         print(LoanAmount)
+		response = requests.post('http://localhost:8000/predict', json=data)
+		prediction = response.text
+		st.success(prediction)
 
 if __name__=='__main__':
-	model = load_model_and_predict()
 	run()
